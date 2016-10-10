6b1b05e6372e7688253a86090c92d60bbb0c5d6b
Martin Yde Granath <my@aarhus.dk>
2016-10-10 16:24:33 +0200

    test

--- menu-bot.php
+++ menu-bot.php
@@ -68,18 +68,14 @@ print_r($response_array);
 # You can use any emoji that is available to your Slack team, including the custom ones.
 if($ch_response === FALSE){
   # isitup.org could not be reached 
-  $reply = "Ironically, isitup could not be reached.";
-}else{
-  if($response_array["status_code"] == 1){
-  	# Yay, the domain is up! 
-    $reply = ":thumbsup: I am happy to report that *<http://".$response_array["domain"]."|".$response_array["domain"].">* is *up*!";
-  } else if($response_array["status_code"] == 2){
-    # Boo, the domain is down. 
-    $reply = ":disappointed: I am sorry to report that *<http://".$response_array["domain"]."|".$response_array["domain"].">* is *not up*!";
-  } else if($response_array["status_code"] == 3){
-    # Uh oh, isitup.org doesn't think the domain entered by the user is valid
-    $reply = ":interrobang: *".$text."* does not appear to be a valid domain. \n";
-    $reply .= "Please enter both the domain name AND suffix (example: *amazon.com* or *whitehouse.gov*).";
+  $reply = "Ironically, Dokk1-menu could not be reached.";
+}
+else{
+  foreach ($response_array as $key => $value) {
+    $timestamp = strtotime($value['date']);
+    $day = date('D', $timestamp);
+    print $day . ': ' . $value['name'];
+    print '--';
   }
 }
 
