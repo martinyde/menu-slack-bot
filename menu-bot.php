6f7b6a2b9f22fb2f551ec277765afce981fa9a2b
Martin Yde Granath <my@aarhus.dk>
2016-10-11 09:22:01 +0200

    Add image

--- menu-bot.php
+++ menu-bot.php
@@ -81,7 +81,7 @@ else {
       'value' => $value['name'] . ' ' . $value['details'],
       'short' => false,
     );
-    print $day . '--';
+    print '---' . $value['images']['0'];
     $daily_img = ($day == $current_day) ? $value['images']['0'] : 'http://dokk1.dk/sites/all/themes/dokk/logo.png';
   }
 
